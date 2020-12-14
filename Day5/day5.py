e6801537ef26d6de1b30fe1fb2d2bba27ed78892
Daniel Gellerman <daniel@gellerman.nu>
2020-12-14 10:37:38 +0100

    day 7 done

--- Day5/day5.py
+++ Day5/day5.py
@@ -11,22 +11,59 @@ Created on Sat Dec  5 21:07:11 2020
 seats = []
 with open('input.txt', 'r') as f:
     for l in f.readlines():
-        seats.append(l)
-
+        seats.append(l.replace('\n', ''))
+# %% Part 1
 def get_pos(seat_code, seat_pos):
-    print(seat_code)
+
+    
+    if (len(seat_pos)) == 1:
+        return seat_pos[0]
+    
     if seat_code[0] in ['F', 'L']:
-        seat_pos_remaining = seat_pos[:len(seat_pos)]
+        seat_pos_remaining = seat_pos[:len(seat_pos)//2]
+        return get_pos(seat_code[1:], seat_pos_remaining)
         
     if seat_code[0] in ['B', 'R']:
-        seat_pos_remaining = seat_pos[:len(seat_pos)]
-    print(seat_pos_remaining)
-    
-    if len(seat_pos_remaining) != 1:
-        seat_pos_remaining = get_pos(seat_code[1:], seat_pos_remaining)
-        
-    return seat_pos_remaining
+        seat_pos_remaining = seat_pos[len(seat_pos)//2:]
+        return get_pos(seat_code[1:], seat_pos_remaining)
 
-print(get_pos(seats[0][0:-3], [range(128)]))
+
+max_value = 0
+for seat in seats:
     
+    row = get_pos(seat[0:-3], list(range(128)))
+    column = get_pos(seat[-3:], list(range(8)))
     
+    value = row * 8 + column
+    
+    if value > max_value:
+        max_value = value
+
+
+# %% Part 2
+        
+seat_ids = []
+    
+for seat in seats:
+
+    row = get_pos(seat[0:-3], list(range(128)))
+    column = get_pos(seat[-3:], list(range(8)))
+    
+    value = row * 8 + column
+    
+    seat_ids.append(value)
+    
+sorted_seats = sorted(seat_ids)
+
+new = 0
+old = 0
+
+for i in sorted_seats:
+    if not (i+1 in sorted_seats and i-1 in sorted_seats):
+        new = i
+        
+        if new - old == 2:
+            my_seat = old +1
+            print(my_seat)
+        old = new
+        
